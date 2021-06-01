729f977a4ffd5994182d1676a3d74420bc0407fd
nskotow <nskotow@icloud.com>
2021-06-01 11:39:53 +0300

    status

--- src/utls/weather.js
+++ src/utls/weather.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const weather = (location, callback) => {
-    const url = "http://api.weatherstack.com/current?access_key=c077671266f689b2c5f06e525449b3fc&query="+location+"&units=f";
+    const url = "http://api.weatherstack.com/current?access_key=c077671266f689b2c5f06e525449b3fc&query="+location;
     request({url: url, json: true}, (error, response) => {
         if (error) {
             callback('Something went wrong', undefined)
